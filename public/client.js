2864224d7dfb22e9c53cb5d0164b79874f953f6c
Christian Hager <christianhager@Christian-Hagers-MacBook-Pro-2.local>
2010-12-16 17:49:48 +0100

    init

--- public/client.js
+++ public/client.js
@@ -2,8 +2,9 @@
 
 
 var Redisio = function(options){
-	this.host = options.host;
-	this.port = options.port;
+	this.options = options;
+	this.host = this.options.host;
+	this.port = this.options.port;
 	
 	this.socket = new io.Socket(this.host,
     {rememberTransport: false, port: this.port, secure: this.options.secure}
