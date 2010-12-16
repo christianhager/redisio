084adfd3f8530291cac0d3b05372042426770acc
Christian Hager <christianhager@Christian-Hagers-MacBook-Pro-2.local>
2010-12-16 16:08:03 +0100

    init

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var http = require('http')
   , url = require('url')
   , fs = require('fs')
   , io = require('./vendor/socket.io.server')
-  , nstatic  = require("./vendor/node-static/lib")
+	, nstatic = require("node-static");
   , sys = require('sys')
   , server;
     
